a4f14df604f9945b48c5958d2cd128c5154a459d
Yu Li <ylilarry@gmail.com>
2021-04-05 01:37:19 -0400

    reformat tests

--- tests/dcf-backend-example/dcf-backend-example-proj/dcf_backend_example/settings.py
+++ tests/dcf-backend-example/dcf-backend-example-proj/dcf_backend_example/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "i83&&wh$n^$*0m92_&7hna4ldn@zrxj+x495psq*m(98ee8*6d"
+SECRET_KEY = "8%(32qj(!wpi-rfh4t6s53y^nca8fu)8q95#t6o+*(tghh#r)s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
